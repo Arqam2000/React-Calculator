376d9938827e2a4d054f03370537cc5253978ec9
Arqam2000 <126051128+Arqam2000@users.noreply.github.com>
2023-06-12 12:29:21 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import React, { useState } from 'react'
-import { Buttons } from "./components/Buttons"
 import { Display } from "./components/Display"
 import "./components/style.css"
 
